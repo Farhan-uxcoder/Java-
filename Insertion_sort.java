87daadd47860478c131a9c412d1bac57e6523c35
Mohammed Farhan <mohammedfarhan1089@gmail.com>
2025-11-15 22:57:00 +0530

    Added Insertion sort

---
+++ Insertion_sort.java
@@ -0,0 +1,24 @@
+public class Insertion_sort {
+    public static void main(String args[]){
+        int arr [] = {7,8,3,1,2};
+        System.out.println("Before sorting ");
+        for (int i =0 ;i< arr.length;i++){
+            System.out.print(" " + arr[i]);
+        }
+
+        for(int i=0;i<arr.length;i++){
+            int current = arr[i];
+            int j = i - 1;
+            while(j >= 0 && current < arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            //placement
+            arr[j+1]=current;
+        }
+        System.out.println("\nafter sorting : ");
+        for (int i =0 ;i< arr.length;i++){
+            System.out.print(" "+arr[i]);
+        }
+    }
+}
