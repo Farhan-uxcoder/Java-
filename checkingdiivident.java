aff5015c35e7e757211ec36e0108ec3993f9f1fa
Mohammed Farhan <mohammedfarhan1089@gmail.com>
2025-11-20 23:16:13 +0530

    added checking divident

---
+++ checkingdiivident.java
@@ -0,0 +1,17 @@
+public class checkingdiivident {
+    public static void main(String args []){
+        int arr[] ={2,4,6,8};
+        int divisor = 2;
+        int quo = 1;
+        int remainder =1;
+        int divident = divisor*quo+remainder;
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]==divident){
+                System.out.println(i);
+                break;
+ }System.out.println("-1");
+            break;
+
+        }
+    }
+}
